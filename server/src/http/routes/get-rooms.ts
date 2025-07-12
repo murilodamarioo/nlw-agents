bf83d0e20cb2b0bcf074f6be1e9d0e6a49d734e3
Murilo Damario <murilodamario@macgroot.local>
2025-07-11 21:05:50 -0300

    :sparkles:feat: Applies count for questions

--- server/src/http/routes/get-rooms.ts
+++ server/src/http/routes/get-rooms.ts
@@ -1,6 +1,7 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { db } from '../../db/connections.ts'
 import { schema } from '../../db/schema/index.ts'
+import { count, eq } from 'drizzle-orm'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get('/rooms', async () => {
@@ -9,8 +10,11 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
         id: schema.rooms.id,
         name: schema.rooms.name,
         description: schema.rooms.description,
+        questionsCount: count(schema.questions.id)
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.createdAt)
 
       return response
